1a65751a4bec2a48b41a1c3e14c330274ec03e6a
StevieGerald <steviegerald007@gmail.com>
2026-07-02 00:37:48 +0100

    updated to ESM

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-const express = require("express");
-
-const recipeRoutes = require("./routes/recipeRoutes");
+import express from "express";
+import recipeRoutes from "./routes/recipeRoutes.js";
 
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(express.json());
 
-// Home route
 app.get("/", (req, res) => {
   res.send("Welcome to Recipe Manager API");
 });
 
-// Recipe routes
 app.use("/recipes", recipeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
