663fbb19d303bbf7a6f671fb54b7024048908204
Anthony Chukwuemeka Eze <mikeyeze76@gmail.com>
2026-07-02 14:20:24 +0000

    Add recipe validation middleware

---
+++ middleware/validateRecipe.js
@@ -0,0 +1,29 @@
+function validateRecipe(req, res, next) {
+    const errors = [];
+    const { id, name, category, ingredients, instructions } = req.body;
+// checking if the id field is valid
+    if ( typeof id !== 'number') {
+        errors.push('Invalid or missing recipe ID.');
+    }
+    //checking if the name field is valid
+    if (typeof name !== 'string' || name.trim() === '') {
+        errors.push('Name is required and must be a text string.');
+    }
+   // checking if the category field is valid
+    if (typeof category !== 'string' || category.trim() === '') {
+        errors.push('Category is required and must be a text string.');
+    }
+    //checking if the ingredients field is valid
+    if (ingredients.length === 0 || !Array.isArray(ingredients)) {
+        errors.push('Ingredients is required and must be an array.');
+    }
+    //checking if the instructions field is valid
+    if (typeof instructions !== 'string' || instructions.trim() === '') {
+        errors.push('Instructions is required and must be a text string.');
+    }
+if (errors.length > 0) {
+        return res.status(400).json({ errors });
+    }
+    next();
+}
+export default validateRecipe; 
